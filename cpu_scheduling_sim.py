dbf66d9af0f891847552961da4b0276e5adf1143
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2026-05-29 14:29:56 +0800

    create CPUSchedulingSim class

---
+++ cpu_scheduling_sim.py
@@ -0,0 +1,15 @@
+import customtkinter
+
+class CPUSchedulingSim:
+    def __init__(self, app):
+        self.app = app
+        self.app.title("CPU Scheduling Simulator")
+        self.app.geometry("1280x720")
+
+        
+
+
+
+
+
+
