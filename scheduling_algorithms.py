63ab60b3ae8bed218de743700cb8eb776dfa67a7
MingMing0000 <120778618+MingMing0000@users.noreply.github.com>
2026-05-31 20:07:41 +0800

    create file to store scheduling algorithms

--- scheduling_algorithms.py
+++ scheduling_algorithms.py
@@ -1,4 +1,30 @@
 
-
 class SchedulingAlgorithms:
+    @staticmethod
+    def simulate_fcfs(processes):
+        # Sort by arrival time
+        procs = sorted(processes, key=lambda x: x['at'])
+        current_time = 0
+        schedule = []
+        total_tat = 0
+        total_wt = 0
+
+        for p in procs:
+            if current_time < p['at']:
+                current_time = p['at'] 
+            
+            start_time = current_time
+            current_time += p['bt']
+            
+            tat = current_time - p['at']
+            wt = tat - p['bt']
+            
+            total_tat += tat
+            total_wt += wt
+            
+            schedule.append((p['name'], start_time, current_time))
+
+        n = len(procs)
+        return schedule, total_tat / n, total_wt / n
+
     
